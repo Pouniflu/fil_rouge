50f6e8c4ece2484ace8876af0fb5abd97235e721
Claire <claire.brisbart@edu.devinci.fr>
2020-12-07 17:34:22 +0100

    :pencil2: Fix typo Lato

--- App/View/header.php
+++ App/View/header.php
@@ -4,6 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="assets/css/import.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
         <title>Mozza</title>
     </head>
     <body>
