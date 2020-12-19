8992e213c07fa45248b7d00f38407bf65c27576e
Claire <claire.brisbart@edu.devinci.fr>
2020-12-19 16:28:43 +0100

    :hammer: Update JS for profil

--- Public/assets/js/main.js
+++ Public/assets/js/main.js
@@ -1,14 +1,52 @@
 // Page de profil
 // Liste d'amis
-
-$("#open-list").click(function(){
+$("#open-list-friend").click(function(){
     $(".profil-main-list-friend").css("display", "flex");
     $(this).css("display", "none");
-    $("#close-list").css("display", "block");
+    $("#close-list-friend").css("display", "block");
 });
 
-$("#close-list").click(function(){
+$("#close-list-friend").click(function(){
     $(".profil-main-list-friend").css("display", "none");
     $(this).css("display", "none");
-    $("#open-list").css("display", "block");
+    $("#open-list-friend").css("display", "block");
+});
+
+// Paris en cours
+$("#open-list-pari").click(function(){
+    $(".profil-main-list-pari").css("display", "flex");
+    $(this).css("display", "none");
+    $("#close-list-pari").css("display", "block");
+});
+
+$("#close-list-pari").click(function(){
+    $(".profil-main-list-pari").css("display", "none");
+    $(this).css("display", "none");
+    $("#open-list-pari").css("display", "block");
+});
+
+// Anciens paris
+$("#open-list-lastPari").click(function(){
+    $(".profil-main-list-lastPari").css("display", "block");
+    $(this).css("display", "none");
+    $("#close-list-lastPari").css("display", "block");
+});
+
+$("#close-list-lastPari").click(function(){
+    $(".profil-main-list-lastPari").css("display", "none");
+    $(this).css("display", "none");
+    $("#open-list-lastPari").css("display", "block");
+});
+
+// Paramètres
+$("#open-list-param").click(function(){
+    $(".profil-main-list-param").css("display", "flex");
+    $(this).css("display", "none");
+    $("#close-list-param").css("display", "block");
+});
+
+$("#close-list-param").click(function(){
+    $(".profil-main-list-param").css("display", "none");
+    $(this).css("display", "none");
+    $("#open-list-param").css("display", "block");
 });
