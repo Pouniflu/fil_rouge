9f5b3a474f77bfb6b09928583e54aec4ab7c9dcc
Claire <claire.brisbart@edu.devinci.fr>
2020-12-21 23:53:30 +0100

    :zap: Set Interval for Ajax

--- Public/assets/js/votes.js
+++ Public/assets/js/votes.js
@@ -14,48 +14,52 @@ function $_GET(param) {
     return vars;
 }
 
-$.ajax({
+function score(){
+    $.ajax({
 
-    url:"?page=poll&sondage_id=" + $_GET("sondage_id") + "&function=vote",
-    dataType:"json",
-    success:function(response){
-        console.log(response);
-        $vote1 = response[0]['votes'];
-        $vote2 = response[1]['votes'];
-        $vote3 = response[2]['votes'];
-        $vote4 = response[3]['votes'];
-
-        $('#div1').html($vote1);
-        $('#div2').html($vote2);
-        $('#div3').html($vote3);
-        $('#div4').html($vote4);
-
-        $(".submit1").click(function(){
+        url:"?page=poll&sondage_id=" + $_GET("sondage_id") + "&function=vote",
+        dataType:"json",
+        success:function(response){
+            console.log(response);
+            $vote1 = response[0]['votes'];
+            $vote2 = response[1]['votes'];
+            $vote3 = response[2]['votes'];
+            $vote4 = response[3]['votes'];
+    
             $('#div1').html($vote1);
             $('#div2').html($vote2);
             $('#div3').html($vote3);
             $('#div4').html($vote4);
-        });
-        $(".submit2").click(function(){
-            $('#div1').html($vote1);
-            $('#div2').html($vote2);
-            $('#div3').html($vote3);
-            $('#div4').html($vote4);
-        });
-        $(".submit3").click(function(){
-            $('#div1').html($vote1);
-            $('#div2').html($vote2);
-            $('#div3').html($vote3);
-            $('#div4').html($vote4);
-        });
-        $(".submit4").click(function(){
-            $('#div1').html($vote1);
-            $('#div2').html($vote2);
-            $('#div3').html($vote3);
-            $('#div4').html($vote4);
-        });
-    }
+    
+            $(".submit1").click(function(){
+                $('#div1').html($vote1);
+                $('#div2').html($vote2);
+                $('#div3').html($vote3);
+                $('#div4').html($vote4);
+            });
+            $(".submit2").click(function(){
+                $('#div1').html($vote1);
+                $('#div2').html($vote2);
+                $('#div3').html($vote3);
+                $('#div4').html($vote4);
+            });
+            $(".submit3").click(function(){
+                $('#div1').html($vote1);
+                $('#div2').html($vote2);
+                $('#div3').html($vote3);
+                $('#div4').html($vote4);
+            });
+            $(".submit4").click(function(){
+                $('#div1').html($vote1);
+                $('#div2').html($vote2);
+                $('#div3').html($vote3);
+                $('#div4').html($vote4);
+            });
+        }
+    
+    }).fail(function(response){
+        console.log(response.statusText)
+    })
+}
 
-}).fail(function(response){
-    console.log(response.statusText)
-})
+setInterval(score, 5000);
